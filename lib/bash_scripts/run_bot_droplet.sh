ddec460899084066ef5723745216edbd0eb05054
willwoodlief <willwoodlief@gmail.com>
2018-09-08 02:08:06 -0500

    better logging

--- lib/bash_scripts/run_bot_droplet.sh
+++ lib/bash_scripts/run_bot_droplet.sh
@@ -7,4 +7,4 @@ cd /home/bot/htdocs/bot
 # load rvm ruby
 source /home/bot/.rvm/environments/ruby-2.5.1@reddit_bot
 
-bundle exec rake  reddit:bot RAILS_ENV="production" &>>  /home/bot/htdocs/bot/log/bot_cron.log
+bundle exec rake  reddit:bot RAILS_ENV="production" 2>&1 >>  /home/bot/htdocs/bot/log/bot_cron.log
